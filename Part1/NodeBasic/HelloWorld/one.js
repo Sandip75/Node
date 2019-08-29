186c059b7e89df371b97c395ee4d1f57c67bb22b
Sandip75 <sk1957339@gmail.com>
2019-08-30 04:48:52 +0530

    module exports two function

--- Part1/NodeBasic/HelloWorld/one.js
+++ Part1/NodeBasic/HelloWorld/one.js
@@ -1,9 +1,21 @@
-//---------Section 1 ---------------------
+//---------Section 1 -----------------------------
 // console.log("Hellow World");
+//--------------------------------------------------
 
-//-----------Section 2 -------------
+//-----------Section 2 -----------------------------
+// var fn = require("./two");
+// //console.log(fn);
+// let result = fn.add(22,23);
+// console.log(result);
+//--------------------------------------------------------
+
+//------------Section 3 -------------------------------------
 var fn = require("./two");
 //console.log(fn);
-let result = fn.add(22,23);
-console.log(result);
+let resultSum = fn.add(20,56);
+let resultSub = fn.sub(45,9);
+console.log(resultSum + "  " + resultSub);
+//----------------------------------------------------------
+
+
 
--- Part1/NodeBasic/HelloWorld/two.js
+++ Part1/NodeBasic/HelloWorld/two.js
@@ -1,5 +1,21 @@
-function addition(a=0 , b = 1){
-    return a + b ;
+//-------------Section 1 -------------------------------------
+// function addition(a=0 , b = 1){
+//     return a + b ;
+// }
+
+// module.exports.add = addition ;
+
+//-------------------------------------------------------------
+
+//------------------Section 2 --------------------------------
+function addition(a=0,b=0){
+    return a+b;
 }
 
-module.exports.add = addition ;
+function subtract(a=0,b=0){
+    return a-b;
+}
+
+module.exports.add = addition;
+module.exports.sub = subtract;
+//------------------------------------------------------------
