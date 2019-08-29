8d2b5a22774ecc8381454f744e039909021833fd
Sandip75 <sk1957339@gmail.com>
2019-08-30 04:35:38 +0530

    module export

--- Part1/NodeBasic/HelloWorld/one.js
+++ Part1/NodeBasic/HelloWorld/one.js
@@ -1,1 +1,9 @@
-console.log("Hellow World");
+//---------Section 1 ---------------------
+// console.log("Hellow World");
+
+//-----------Section 2 -------------
+var fn = require("./two");
+//console.log(fn);
+let result = fn.add(22,23);
+console.log(result);
+
---
+++ Part1/NodeBasic/HelloWorld/two.js
@@ -0,0 +1,5 @@
+function addition(a=0 , b = 1){
+    return a + b ;
+}
+
+module.exports.add = addition ;
