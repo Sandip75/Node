c5d00a9557ced138d1cd7c8b855e009db16e48fc
Sandip75 <sk1957339@gmail.com>
2019-09-03 07:34:33 +0530

    node

---
+++ u/NoteApp/app.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+//fs.writeFileSync('Note.txt','Welcome to node js');  // This will create one file and write in that 
+//fs.writeFileSync('Note.txt','Hi How are you');  // This will update existing file 
+
+//
+// Challenge : Append a messeage in Note.txt
+//
+
+fs.appendFileSync('Note.txt' , 'Where are you');  // This will append the text in Note.txt
